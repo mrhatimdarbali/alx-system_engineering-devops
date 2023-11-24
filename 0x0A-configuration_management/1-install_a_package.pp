70a2103fd9a3c708e84c96f6c6e7099ab4e397fd
hatim darbali <mrhatimdarbali@gmail.com>
2023-11-24 06:39:33 -0800

    o

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# Using Puppt to install flask from pip3
-package {'flask':
+# Using Puppet to install Flask from pip
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
 }
