9e9e7ee96432459642a5914818ac0cee2e2682f2
hatim darbali <mrhatimdarbali@gmail.com>
2023-11-24 05:02:12 -0800

    o

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint -v 2.5.0
+# install install Flask -v 2.1.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+exec { 'install Flask':
+  command => '/usr/bin/apt-get -y install install Flask -v 2.1.0',
 }
