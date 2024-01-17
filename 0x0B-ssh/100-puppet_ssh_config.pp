63acb2ce510bedf8a951db04c451365b1f8f2b9e
hatim darbali <mrhatimdarbali@gmail.com>
2024-01-17 04:57:50 -0800

    o

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,12 @@
-# Seting up my client config file
-include stdlib
-
+# Client configuration file
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
-  replace => true,
+  path   => '/etc/ssh/ssh_config',
 }
 
-file_line { 'Delare identity file':
+file_line { 'Declare identity file':
   ensure => present,
+  line   => '    IdentityFile ~/.ssh/school',
   path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
-  replace => true,
 }
